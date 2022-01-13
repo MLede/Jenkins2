4cae6dd3484db51caf01ebeb9bf2bf4f33cd7333
Marie Ledesma <marie.ledesma@katalon.com>
2022-01-13 15:12:41 +0700

    Commit Jenkins

---
+++ Test Suites/Test Suites2.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Test Suites2</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>35865ec7-f177-40f9-a8e0-1836f0f0d8c7</testSuiteGuid>
+</TestSuiteEntity>
